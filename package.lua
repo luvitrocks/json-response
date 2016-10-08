3b6e84ab59122af487ead9f9dffae0abb0c142d1
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-10-08 14:12:17 +0200

    update package

--- package.lua
+++ package.lua
@@ -1,15 +1,14 @@
-  return {
-    name = "voronianski/json-response",
-    version = "1.0.0",
-    description = "JSON pretty-printed response middleware for Utopia",
-    tags = { "luvit", "utopia", "json", "middleware", "response" },
-    license = "MIT",
-    author = { name = "Dmitri Voronianski", email = "dmitri.voronianski@gmail.com" },
-    homepage = "https://github.com/luvitrocks/json-response",
-    dependencies = {},
-    files = {
-      "**.lua",
-      "!test*"
-    }
+return {
+  name = "voronianski/json-response",
+  version = "1.0.0",
+  description = "JSON pretty-printed response middleware for Utopia",
+  tags = { "luvit", "utopia", "json", "middleware", "response" },
+  license = "MIT",
+  author = { name = "Dmitri Voronianski", email = "dmitri.voronianski@gmail.com" },
+  homepage = "https://github.com/luvitrocks/json-response",
+  dependencies = {},
+  files = {
+    "**.lua",
+    "!test*"
   }
-  
+}
